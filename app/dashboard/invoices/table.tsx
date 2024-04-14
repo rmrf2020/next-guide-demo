458e8019cb7d05006b4e398d050bdbc5c8cf0b20
rmrf2020 <commonhc@outlook.com>
2024-04-14 03:30:51 +0000

    feat:update

---
+++ app/dashboard/invoices/table.tsx
@@ -0,0 +1,13 @@
+// ...
+import {fetchFilteredInvoices} from "@/app/lib/data";
+
+export default async function InvoicesTable({
+                                                query,
+                                                currentPage,
+                                            }: {
+    query: string;
+    currentPage: number;
+}) {
+    const invoices = await fetchFilteredInvoices(query, currentPage);
+    // ...
+}
